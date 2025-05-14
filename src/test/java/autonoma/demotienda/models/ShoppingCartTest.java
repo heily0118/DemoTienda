b7c39862c1240a2d725767c3776224de6e07b895
María Paz Puerta Acevedo <mariap.puertaa@autonoma.edu.co>
2025-05-14 17:54:15 -0500

    Se agregaron casos de prueba

--- src/test/java/autonoma/demotienda/models/ShoppingCartTest.java
+++ src/test/java/autonoma/demotienda/models/ShoppingCartTest.java
@@ -7,6 +7,8 @@ package autonoma.demotienda.models;
 import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 /**
  *
@@ -14,96 +16,26 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class ShoppingCartTest {
     
-    public ShoppingCartTest() {
+    @BeforeEach
+    public void setUp(){
+        System.out.println("Before Each");
     }
-
-    @org.junit.jupiter.api.Test
-    public void testGetUser() {
-        System.out.println("getUser");
-        ShoppingCart instance = null;
-        User expResult = null;
-        User result = instance.getUser();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+    
+    @BeforeAll
+    public void ejecucionInicial(){
+        System.out.println("Before all");
     }
-
-    @org.junit.jupiter.api.Test
-    public void testSetUser() {
-        System.out.println("setUser");
-        User user = null;
-        ShoppingCart instance = null;
-        instance.setUser(user);
-        fail("The test case is a prototype.");
+    
+    @Test
+    public void pruebaExitosa(){
+        System.out.println("Prueba exitosa");
     }
-
-    @org.junit.jupiter.api.Test
-    public void testGetProductos() {
-        System.out.println("getProductos");
-        ShoppingCart instance = null;
-        ArrayList<Product> expResult = null;
-        ArrayList<Product> result = instance.getProductos();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+    
+    @Test
+    public void pruebaErronea(){
+        System.out.println("Prueba erronea");
     }
-
-    @org.junit.jupiter.api.Test
-    public void testSetProductos() {
-        System.out.println("setProductos");
-        ArrayList<Product> productos = null;
-        ShoppingCart instance = null;
-        instance.setProductos(productos);
-        fail("The test case is a prototype.");
-    }
-
-    @org.junit.jupiter.api.Test
-    public void testSearchProduct() {
-        System.out.println("searchProduct");
-        int id = 0;
-        ShoppingCart instance = null;
-        Product expResult = null;
-        Product result = instance.searchProduct(id);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    @org.junit.jupiter.api.Test
-    public void testAgregarProducto() {
-        System.out.println("agregarProducto");
-        Product producto = null;
-        ShoppingCart instance = null;
-        boolean expResult = false;
-        boolean result = instance.agregarProducto(producto);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    @org.junit.jupiter.api.Test
-    public void testEliminarProducto() {
-        System.out.println("eliminarProducto");
-        Product producto = null;
-        ShoppingCart instance = null;
-        boolean expResult = false;
-        boolean result = instance.eliminarProducto(producto);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    @org.junit.jupiter.api.Test
-    public void testObtenerCantidadProductor() {
-        System.out.println("obtenerCantidadProductor");
-        ShoppingCart instance = null;
-        int expResult = 0;
-        int result = instance.obtenerCantidadProductor();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    @org.junit.jupiter.api.Test
-    public void testCompraProducto() {
-        System.out.println("CompraProducto");
-        ShoppingCart instance = null;
-        instance.CompraProducto();
-        fail("The test case is a prototype.");
-    }
+    
+   
     
 }
