afd4a399db0c0693b4e4f9be3f9c032a8834bb45
Heily Rios <heilyayala005@gmail.com>
2025-05-14 17:46:45 -0500

    Se hicieron cambios en test

--- src/test/java/autonoma/demotienda/models/ShoppingCartTest.java
+++ src/test/java/autonoma/demotienda/models/ShoppingCartTest.java
@@ -17,8 +17,8 @@ public class ShoppingCartTest {
     public ShoppingCartTest() {
     }
 
-    @org.junit.jupiter.api.Test
-    public void testGetUser() {
+    @Test
+    public void PruebaExitosa() {
         System.out.println("getUser");
         ShoppingCart instance = null;
         User expResult = null;
@@ -27,83 +27,6 @@ public class ShoppingCartTest {
         fail("The test case is a prototype.");
     }
 
-    @org.junit.jupiter.api.Test
-    public void testSetUser() {
-        System.out.println("setUser");
-        User user = null;
-        ShoppingCart instance = null;
-        instance.setUser(user);
-        fail("The test case is a prototype.");
-    }
-
-    @org.junit.jupiter.api.Test
-    public void testGetProductos() {
-        System.out.println("getProductos");
-        ShoppingCart instance = null;
-        ArrayList<Product> expResult = null;
-        ArrayList<Product> result = instance.getProductos();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    @org.junit.jupiter.api.Test
-    public void testSetProductos() {
-        System.out.println("setProductos");
-        ArrayList<Product> productos = null;
-        ShoppingCart instance = null;
-        instance.setProductos(productos);
-        fail("The test case is a prototype.");
-    }
-
-    @org.junit.jupiter.api.Test
-    public void testSearchProduct() {
-        System.out.println("searchProduct");
-        int id = 0;
-        ShoppingCart instance = null;
-        Product expResult = null;
-        Product result = instance.searchProduct(id);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    @org.junit.jupiter.api.Test
-    public void testAgregarProducto() {
-        System.out.println("agregarProducto");
-        Product producto = null;
-        ShoppingCart instance = null;
-        boolean expResult = false;
-        boolean result = instance.agregarProducto(producto);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    @org.junit.jupiter.api.Test
-    public void testEliminarProducto() {
-        System.out.println("eliminarProducto");
-        Product producto = null;
-        ShoppingCart instance = null;
-        boolean expResult = false;
-        boolean result = instance.eliminarProducto(producto);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    @org.junit.jupiter.api.Test
-    public void testObtenerCantidadProductor() {
-        System.out.println("obtenerCantidadProductor");
-        ShoppingCart instance = null;
-        int expResult = 0;
-        int result = instance.obtenerCantidadProductor();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    @org.junit.jupiter.api.Test
-    public void testCompraProducto() {
-        System.out.println("CompraProducto");
-        ShoppingCart instance = null;
-        instance.CompraProducto();
-        fail("The test case is a prototype.");
-    }
+    
     
 }
